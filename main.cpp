c047bc56d0caa03740ebd533b819d84aceccd6d7
AstralArchitect <matthieu.jlp.poncet@gmail.com>
2024-06-01 12:22:05 +0200

    add a plan

--- main.cpp
+++ main.cpp
@@ -15,9 +15,10 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
+unsigned int loadTexture(const char *path);
 
 // settings
-const unsigned int SCR_WIDTH = 1902;
+const unsigned int SCR_WIDTH = 1920;
 const unsigned int SCR_HEIGHT = 1080;
 
 // camera
@@ -72,30 +73,46 @@ int main()
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
 
+    // build and compile plan shader
     Shader Plan("plan.vs", "plan.fs");
 
     float planVertices[]{
-        //triangle 1
-        -0.5,  0.0,  0.5,
-        -0.5,  0.0, -0.5,
-         0.5,  0.0, -0.5,
-        //triangle 2
-         0.5,  0.0, -0.5,
-         0.5,  0.0,  0.5,
-        -0.5,  0.0,  0.5
+        // positions          // texture coords
+         0.5f, -0.5f,  0.5f,   1.0f, 1.0f, // top right
+         0.5f, -0.5f, -0.5f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, -0.5f,   0.0f, 0.0f, // bottom left
+        -0.5f, -0.5f,  0.5f,   0.0f, 1.0f  // top left 
+    };
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
     };
 
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO;
 
-    glGenBuffers(1, &VBO);
     glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(planVertices), planVertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    //load texture
+    unsigned int texture = loadTexture("res/container2.png");
+
+    //shader configuration
+    Plan.use();
+    Plan.setInt("myTexture", 0);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -125,11 +142,16 @@ int main()
 
         // world transformation
         glm::mat4 model = glm::mat4(1.0f);
+        model = glm::scale(model, glm::vec3(2.0f, 1.0f, 2.0f));
         Plan.setMat4("model", model);
+
+        //bind texture
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
 
         // render the cube
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 18);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -158,6 +180,11 @@ void processInput(GLFWwindow *window)
         camera.ProcessKeyboard(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_F11) == GLFW_PRESS)
+    {
+        //mettre en plain écran
+    }
+    
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -197,4 +224,41 @@ void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
+unsigned int loadTexture(char const * path)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+    if (data)
+    {
+        GLenum format;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
+    }
+    else
+    {
+        printf("Texture failed to load at path: %s", path);
+        stbi_image_free(data);
+    }
+
+    return textureID;
 }
--- plan.fs
+++ plan.fs
@@ -1,1 +1,12 @@
 #version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoords;
+
+uniform sampler2D myTexture;
+
+void main(){
+    vec3 tex = vec3(texture(myTexture, TexCoords));
+
+    FragColor = vec4(tex, 1.0);
+}
