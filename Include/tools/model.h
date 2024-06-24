363add2b10598d766dfb6a7b030f4442e31dedfa
Astral Architect <matthieu.jlp.poncet@gmail.com>
2024-06-24 10:28:50 +0200

    add uniform function in model class

--- Include/tools/model.h
+++ Include/tools/model.h
@@ -18,8 +18,9 @@ private:
     unsigned int width, height;
 
     glm::mat4 model;
-public:
+
     Shader *shader;
+public:
 
     //(à Nautilus-1021)Je te laisse faire cette fonction en Rust. J'ai rajouté la fonction `void Paths(const char* vertexPath, const char* fragmentPath)` dans la classe Shader qui est la même que le constructeur.
 //    Model(const char* vertexShaderPath, const char* fragmentShaderPath, const char* modelPath);
@@ -31,7 +32,33 @@ public:
     void render();
 
     //transformations functions
+    //------------------------------------------------------------------------
     void translate(glm::vec3 newPos);
+    //------------------------------------------------------------------------
     void scale(glm::vec3 scaleVector);
+    //------------------------------------------------------------------------
     void rotate(float angle, glm::vec3 rotateVector);
+
+    // utility uniform functions
+    // ------------------------------------------------------------------------
+    void setBool(const std::string &name, bool value) const;
+    // ------------------------------------------------------------------------
+    void setInt(const std::string &name, int value) const;
+    // ------------------------------------------------------------------------
+    void setFloat(const std::string &name, float value) const;
+    // ------------------------------------------------------------------------
+    void setVec2(const std::string &name, const glm::vec2 &value) const;
+    void setVec2(const std::string &name, float x, float y) const;
+    // ------------------------------------------------------------------------
+    void setVec3(const std::string &name, const glm::vec3 &value) const;
+    void setVec3(const std::string &name, float x, float y, float z) const;
+    // ------------------------------------------------------------------------
+    void setVec4(const std::string &name, const glm::vec4 &value) const;
+    void setVec4(const std::string &name, float x, float y, float z, float w) const;
+    // ------------------------------------------------------------------------
+    void setMat2(const std::string &name, const glm::mat2 &mat) const;
+    // ------------------------------------------------------------------------
+    void setMat3(const std::string &name, const glm::mat3 &mat) const;
+    // ------------------------------------------------------------------------
+    void setMat4(const std::string &name, const glm::mat4 &mat) const;
 };
--- c++_src/libs/tools/model.cpp
+++ c++_src/libs/tools/model.cpp
@@ -92,3 +92,62 @@ void Model::rotate(float angle, glm::vec3 rotateVector)
 {
     model = glm::rotate(model, angle, rotateVector);
 }
+
+// utility uniform functions
+// ------------------------------------------------------------------------
+void Model::setBool(const std::string &name, bool value) const
+{         
+    glUniform1i(glGetUniformLocation(shader->ID, name.c_str()), (int)value); 
+}
+// ------------------------------------------------------------------------
+void Model::setInt(const std::string &name, int value) const
+{ 
+    glUniform1i(glGetUniformLocation(shader->ID, name.c_str()), value); 
+}
+// ------------------------------------------------------------------------
+void Model::setFloat(const std::string &name, float value) const
+{ 
+    glUniform1f(glGetUniformLocation(shader->ID, name.c_str()), value); 
+}
+// ------------------------------------------------------------------------
+void Model::setVec2(const std::string &name, const glm::vec2 &value) const
+{ 
+    glUniform2fv(glGetUniformLocation(shader->ID, name.c_str()), 1, &value[0]); 
+}
+void Model::setVec2(const std::string &name, float x, float y) const
+{ 
+    glUniform2f(glGetUniformLocation(shader->ID, name.c_str()), x, y); 
+}
+// ------------------------------------------------------------------------
+void Model::setVec3(const std::string &name, const glm::vec3 &value) const
+{ 
+    glUniform3fv(glGetUniformLocation(shader->ID, name.c_str()), 1, &value[0]); 
+}
+void Model::setVec3(const std::string &name, float x, float y, float z) const
+{ 
+    glUniform3f(glGetUniformLocation(shader->ID, name.c_str()), x, y, z); 
+}
+// ------------------------------------------------------------------------
+void Model::setVec4(const std::string &name, const glm::vec4 &value) const
+{ 
+    glUniform4fv(glGetUniformLocation(shader->ID, name.c_str()), 1, &value[0]); 
+}
+void Model::setVec4(const std::string &name, float x, float y, float z, float w) const
+{ 
+    glUniform4f(glGetUniformLocation(shader->ID, name.c_str()), x, y, z, w); 
+}
+// ------------------------------------------------------------------------
+void Model::setMat2(const std::string &name, const glm::mat2 &mat) const
+{
+    glUniformMatrix2fv(glGetUniformLocation(shader->ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+// ------------------------------------------------------------------------
+void Model::setMat3(const std::string &name, const glm::mat3 &mat) const
+{
+    glUniformMatrix3fv(glGetUniformLocation(shader->ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+// ------------------------------------------------------------------------
+void Model::setMat4(const std::string &name, const glm::mat4 &mat) const
+{
+    glUniformMatrix4fv(glGetUniformLocation(shader->ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
