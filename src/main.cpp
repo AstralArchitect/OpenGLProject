c8481638d1fbb6a32f98d6002cce7119bfba9a81
AstralArchitect <none>
2025-03-15 19:24:31 +0100

    Revert "changed background color"
    This reverts commit 4130d9e801f3c942a33b6d554fd55ee921942977.

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ glm::vec3 lightPos(1.0/*cos(0)*/, 1.0, 0.0f/*sin(0)*/);
 glm::vec3 pointLightColor = glm::vec3(1.0f, 0.9f, 0.8f);
 
 // background strength
-glm::vec3 backgroundColor(0.2f, 0.2f, 0.2f);
+glm::vec3 backgroundColor(0.4f, 0.4f, 0.4f);
 
 std::vector<Light> lights;
 
